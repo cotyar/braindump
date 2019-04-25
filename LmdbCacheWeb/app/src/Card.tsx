336d4c8d1c4fd87e294a5606866c11da76443669
cotyar <khabarov@gmail.com>
2019-04-25 15:18:02 +0100

    MonitoringServiceClient enabling attempt

--- LmdbCacheWeb/app/src/Card.tsx
+++ LmdbCacheWeb/app/src/Card.tsx
@@ -1,7 +1,14 @@
 import React from 'react'; // we need this to make JSX compile
-import { LightningConfig } from "./generated/lmdb_cache_remoting_pb";
 import proto from 'google-protobuf'
 import {grpc} from "@improbable-eng/grpc-web";
+
+import { LightningConfig, MonitoringUpdateRequest } from "./generated/lmdb_cache_remoting_pb";
+
+import {
+  MonitoringServiceClient,
+  MonitoringService,
+  MonitoringServiceGetStatus
+} from "./generated/lmdb_cache_remoting_pb_service";
 
 const msg = new LightningConfig();
 msg.setName("John Doe");
@@ -10,6 +17,14 @@ type CardProps = {
   title: string,
   paragraph: string
 }
+
+const client = new MonitoringServiceClient("http://localhost:41051");
+const req = new MonitoringUpdateRequest();
+client.getStatus(req, (err, cacheStatus) => {
+  /* ... */
+  console.log(cacheStatus);
+  console.error(err);
+});
 
 export const Card = ({ title, paragraph }: CardProps) => <aside>
   <h2>{ title }</h2>
